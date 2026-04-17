4bce1872847c8894b810833459d61cf9ba280268
contactoagrovisionco-dotcom <contactoagrovision@gmail.com>
2026-04-16 23:13:33 -0300

    arreglo API KEY

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   "projectId": "studio-1014760813-be189",
   "appId": "1:772501385922:web:739fa93cd99c91c393c15b",
   "storageBucket": "studio-1014760813-be189.firebasestorage.app",
-  "apiKey": "AIzaSyAIIOa4Sz_lyPVHyqGjNm3Px13XjlvxDlY",
+  "apiKey": "AIzaSyDECON6YrcAh9R8M8prlaYt937dFyNjnN4",
   "authDomain": "studio-1014760813-be189.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "772501385922"
