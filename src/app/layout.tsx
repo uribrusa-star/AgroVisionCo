84e3bb0d5ada86007ae861736327a8e169fbdf25
contactoagrovisionco-dotcom <contactoagrovision@gmail.com>
2026-08-05 00:38:38 -0300

    fix: remove SWUnregister from root layout to allow service worker to function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AppContextProvider } from '@/context/app-data-context.tsx';
 import { ThemeProvider } from "@/components/theme-provider";
-import { SWUnregister } from '@/components/sw-unregister';
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || 'https://agrovista.com.ar'),
@@ -58,7 +57,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <AppContextProvider>
-            <SWUnregister />
             {children}
             <Toaster />
           </AppContextProvider>
